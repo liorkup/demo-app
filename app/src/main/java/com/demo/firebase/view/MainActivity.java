d1146557d41e073e84dac56f7cb12f20225d2e00
Lior Kupfer <liorkup@gmail.com>
2020-03-22 10:32:22 +0200

    set DDL

--- app/src/main/java/com/demo/firebase/view/MainActivity.java
+++ app/src/main/java/com/demo/firebase/view/MainActivity.java
@@ -2,10 +2,13 @@ package com.demo.firebase.view;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
@@ -24,6 +27,22 @@ import static com.demo.firebase.model.Section.RECOMMENDATION;
 
 public class MainActivity extends AppCompatActivity {
 
+    private SharedPreferences preferences;
+    private SharedPreferences.OnSharedPreferenceChangeListener deepLinkListener;
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        preferences.registerOnSharedPreferenceChangeListener(deepLinkListener);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        preferences.unregisterOnSharedPreferenceChangeListener(deepLinkListener);
+        deepLinkListener = null;
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -33,7 +52,41 @@ public class MainActivity extends AppCompatActivity {
         renderProducts(NEW_ARRIVAL, newArrivalView);
         RecyclerView recommendView = findViewById(R.id.recommendation_view);
         renderProducts(RECOMMENDATION, recommendView);
+        preferences =
+                getSharedPreferences("google.analytics.deferred.deeplink.prefs", MODE_PRIVATE);
+
+        deepLinkListener = (sharedPreferences, key) -> {
+            Log.d("DEEPLINK_LISTENER", "Deep link changed");
+            if ("deeplink".equals(key)) {
+                String deeplink = sharedPreferences.getString(key, null);
+                Double cTime = Double.longBitsToDouble(sharedPreferences.getLong("timestamp", 0));
+                Log.d("DEEPLINK_LISTENER", "Deep link retrieved: " + deeplink);
+                showDeepLinkResult(deeplink);
+            }
+        };
     }
+
+    public void showDeepLinkResult(String result) {
+        String toastText = result;
+        if (toastText == null) {
+            toastText = "The deep link retrieval failed";
+        } else if (toastText.isEmpty()) {
+            toastText = "Deep link empty";
+        }
+        Toast.makeText(MainActivity.this, toastText, Toast.LENGTH_LONG).show();
+        Log.d("DEEPLINK", toastText);
+
+        try {
+            Product product = new MockInventory()
+                    .getProductByName(result.substring(result.lastIndexOf("/") + 1));
+            ProductActivity.navigateToProductPage(MainActivity.this, product.id);
+            Log.d("DEEPLINK", "Showing item from Deep Link");
+        } catch (NullPointerException e) {
+            Log.d("DEEPLINK", "Invalid Deep Link or item name not found");
+        }
+
+    }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
